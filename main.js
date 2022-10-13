477a9306c13f12d4cc3c48d62c2a37468ab00b92
coderaditisingh <86613587+coderaditisingh@users.noreply.github.com>
2022-10-13 11:53:01 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+function transport()
+{
+    window.location="homepage2.html";
+}
+
+function move()
+{
+    window.location="bedroom.html";
+}
+function teleport()
+{
+    window.location="tv and ac.html";
+}
+function back()
+{
+    window.location="homepage2.html";
+}
